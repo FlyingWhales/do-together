a6f77d5eb003ea510e8da663ae59c5cab88f070c
FlyingWhales <ce.cemucar@gmail.com>
2019-11-04 21:30:23 +0300

    Removed unused lines. Order annotations to improve readability. Still I rise.

--- src/main/java/com/doto/app/DoTogetherApplication.java
+++ src/main/java/com/doto/app/DoTogetherApplication.java
@@ -7,19 +7,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.doto.entity.User;
-
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.doto.entity")
-@EnableJpaRepositories(basePackages = "com.doto.repository")
 @EnableJpaAuditing(setDates = true)
 @ComponentScan(basePackages = "com.doto.*")
+@EntityScan(basePackages = "com.doto.entity")
+@EnableJpaRepositories(basePackages = "com.doto.repository")
 public class DoTogetherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoTogetherApplication.class, args);
-		User u = new User();
 	}
 
 }
